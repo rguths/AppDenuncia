bed6bc6fc5d6bb957125fa467fed3f3a3ad6bda8
Rogerio Guths <31018271+rguths@users.noreply.github.com>
2022-10-06 10:25:30 -0400

    Create index.tsx

---
+++ testes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+ 
+export function GitHubAction() {
+  return (
+    <View>
+      <Text> Teste Autonatizado;
+      </Text>
+    </View>
+    );
+}
